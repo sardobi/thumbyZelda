7cb39e9e29cb0b178070b8e35a26a9637fec8082
sardobi <sardobi@googlemail.com>
2025-12-27 21:47:14 +0000

    Add some Player logic for movement

--- zelda.py
+++ zelda.py
@@ -22,6 +22,60 @@ class SpriteBitmaps:
         Left = SpriteBitmap(8, 8, bytearray([0,4,238,157,157,137,247,0]))
         Right = SpriteBitmap(8, 8, bytearray([0,247,137,157,157,238,4,0]))
 
+class Direction:
+    __value__: int
+    
+    def __init__(self, value: int):
+        self.__value__ = value
+
+class Directions:
+    Up = Direction(1)
+    Down = Direction(2)
+    Left = Direction(3)
+    Right = Direction(4)
+
+class Player:
+    xPos: int
+    yPos: int
+    facing: Direction
+    
+    _moveSpeed: int = 1
+    
+    _sprite: thumby.Sprite
+    
+    __directions_to_sprite__: dict[Direction, thumby.Sprite] = {
+        Directions.Up: SpriteBitmaps.Link.Up.to_sprite(),
+        Directions.Down: SpriteBitmaps.Link.Down.to_sprite(),
+        Directions.Left: SpriteBitmaps.Link.Left.to_sprite(),
+        Directions.Right: SpriteBitmaps.Link.Right.to_sprite(),
+    }
+    
+    def __init__(self, xPos: int, yPos: int, facing: Direction):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.facing = facing
+        
+        self._sprite = self.__directions_to_sprite__[facing]
+    
+    def draw(self):
+        thumby.display.drawSprite(self._sprite)
+        
+    def move(self, direction: Direction):
+        if direction == Directions.Up:
+            self.yPos += self._moveSpeed
+        elif direction == Directions.Down:
+            self.yPos -= self._moveSpeed
+        elif direction == Directions.Left:
+            self.xPos -= self._moveSpeed
+        elif direction== Directions.Right:
+            self.xPos += self._moveSpeed
+        else:
+            raise Exception("Unknown direction")
+            
+        self._sprite = self.__directions_to_sprite__[direction]
+        self._sprite.x = self.xPos
+        self._sprite.y = self.yPos
+    
 # Set the FPS (without this call, the default fps is 30)
 thumby.display.setFPS(60)
 
