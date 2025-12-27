63ab49877b3092393402fa79d9f69a739e0bfc44
sardobi <sardobi@googlemail.com>
2025-12-27 20:40:43 +0000

    Replace Thumby demo sprite with Link sprite

--- zelda.py
+++ zelda.py
@@ -17,15 +17,12 @@ class SpriteBitmap:
 
 class SpriteBitmaps:
     class Link:
-        Forward = SpriteBitmap(32, 32, bytearray([0,0,0,0,0,0,0,0,248,8,232,40,40,40,40,40,40,40,40,40,40,232,8,248,0,0,0,0,0,0,0,
-                         0,0,0,0,0,0,0,0,0,255,0,63,32,32,32,32,32,32,32,32,32,32,63,0,255,0,0,0,0,0,0,0,
-                         0,0,0,0,0,0,0,0,0,255,0,12,12,63,63,12,12,0,0,24,24,3,3,0,255,0,0,0,0,0,0,0,0,0,
-                         0,0,0,0,0,0,0,31,16,16,16,16,20,18,16,20,18,16,16,16,16,16,31,0,0,0,0,0,0,0,0]))
+        Forward = SpriteBitmap(8, 8, bytearray([98,244,138,145,145,138,244,98]))
 
 # Set the FPS (without this call, the default fps is 30)
 thumby.display.setFPS(60)
 
-thumbySprite = SpriteBitmaps.Link.Forward.to_sprite()
+linkSprite = SpriteBitmaps.Link.Forward.to_sprite()
 
 while(1):
     t0 = time.ticks_ms()   # Get time (ms)
@@ -38,9 +35,9 @@ while(1):
     bobOffset = math.sin(t0 / bobRate) * bobRange
 
     # Center the sprite using screen and bitmap dimensions and apply bob offset
-    thumbySprite.x = int((thumby.display.width/2) - (32/2))
-    thumbySprite.y = int(round((thumby.display.height/2) - (32/2) + bobOffset))
+    linkSprite.x = int((thumby.display.width/2) - (linkSprite.width/2))
+    linkSprite.y = int(round((thumby.display.height/2) - (linkSprite.width/2) + bobOffset))
 
     # Display the bitmap using bitmap data, position, and bitmap dimensions
-    thumby.display.drawSprite(thumbySprite)
+    thumby.display.drawSprite(linkSprite)
     thumby.display.update()
