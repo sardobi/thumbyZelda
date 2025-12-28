a9d610f1f64ee395691ee52bbb24d57b6080fe96
sardobi <sardobi@googlemail.com>
2025-12-28 01:46:28 +0000

    Implement attack

--- zelda.py
+++ zelda.py
@@ -1,6 +1,11 @@
 import time
 import thumby
 
+GAME_SPEED: int = 60
+PLAYER_ATTACK_COOLDOWN: int = int(GAME_SPEED / 3)
+PROJECTILE_SWORD_LIFETIME: int = GAME_SPEED * 2
+PLAYER_BASE_HEALTH: int = 3
+
 
 class SpriteBitmap:
     xDimension: int
@@ -23,6 +28,12 @@ class SpriteBitmaps:
         Left = SpriteBitmap(8, 8, bytearray([0, 4, 238, 157, 157, 137, 247, 0]))
         Right = SpriteBitmap(8, 8, bytearray([0, 247, 137, 157, 157, 238, 4, 0]))
 
+    class Sword:
+        Up = SpriteBitmap(8, 8, bytearray([0, 0, 0, 32, 254, 32, 0, 0]))
+        Down = SpriteBitmap(8, 8, bytearray([0, 0, 4, 127, 4, 0, 0, 0]))
+        Left = SpriteBitmap(8, 8, bytearray([0, 16, 16, 16, 16, 56, 16, 16]))
+        Right = SpriteBitmap(8, 8, bytearray([16, 16, 56, 16, 16, 16, 16, 0]))
+
 
 class Direction:
     __value__: int
@@ -38,28 +49,8 @@ class Directions:
     Right = Direction(4)
 
 
-class Player:
-    xPos: int
-    yPos: int
-    facing: Direction
-
-    _moveSpeed: int = 1
-
+class Drawable:
     _sprite: thumby.Sprite
-
-    __directions_to_sprite__: dict[Direction, thumby.Sprite] = {
-        Directions.Up: SpriteBitmaps.Link.Up.to_sprite(),
-        Directions.Down: SpriteBitmaps.Link.Down.to_sprite(),
-        Directions.Left: SpriteBitmaps.Link.Left.to_sprite(),
-        Directions.Right: SpriteBitmaps.Link.Right.to_sprite(),
-    }
-
-    def __init__(self, xPos: int, yPos: int, facing: Direction):
-        self.xPos = xPos
-        self.yPos = yPos
-        self.facing = facing
-
-        self._sprite = self.__directions_to_sprite__[facing]
 
     def width(self) -> int:
         return self._sprite.width
@@ -70,31 +61,134 @@ class Player:
     def draw(self):
         thumby.display.drawSprite(self._sprite)
 
+
+class Player(Drawable):
+    xPos: int
+    yPos: int
+    facing: Direction
+
+    health: int
+    max_health: int
+
+    _move_speed: int = 1
+    _attack_cooldown: int = 0
+
+    __directions_to_sprite__: dict[Direction, thumby.Sprite]
+
+    def __init__(self, xPos: int, yPos: int, facing: Direction):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.facing = facing
+        self.health = PLAYER_BASE_HEALTH
+        self.max_health = PLAYER_BASE_HEALTH
+
+        self.__directions_to_sprite__ = {
+            Directions.Up: SpriteBitmaps.Link.Up.to_sprite(),
+            Directions.Down: SpriteBitmaps.Link.Down.to_sprite(),
+            Directions.Left: SpriteBitmaps.Link.Left.to_sprite(),
+            Directions.Right: SpriteBitmaps.Link.Right.to_sprite(),
+        }
+        self._sprite = self.__directions_to_sprite__[facing]
+        self._sprite.x = self.xPos
+        self._sprite.y = self.yPos
+
+    def step(self):
+        if self._attack_cooldown > 0:
+            self._attack_cooldown -= 1
+
     def move(self, direction: Direction):
         if direction == Directions.Up:
-            self.yPos -= self._moveSpeed
+            self.yPos -= self._move_speed
         elif direction == Directions.Down:
-            self.yPos += self._moveSpeed
+            self.yPos += self._move_speed
         elif direction == Directions.Left:
-            self.xPos -= self._moveSpeed
+            self.xPos -= self._move_speed
         elif direction == Directions.Right:
-            self.xPos += self._moveSpeed
+            self.xPos += self._move_speed
         else:
             raise Exception("Unknown direction")
 
         self._sprite = self.__directions_to_sprite__[direction]
         self._sprite.x = self.xPos
         self._sprite.y = self.yPos
+        self.facing = direction
+
+    def attack(self):
+        if self._attack_cooldown > 0:
+            return
+
+        if self.health == self.max_health:
+            swords.append(
+                Sword(self.xPos, self.yPos, self.facing, PROJECTILE_SWORD_LIFETIME)
+            )
+        else:
+            swords.append(Sword(self.xPos, self.yPos, self.facing))
+
+        self._attack_cooldown = PLAYER_ATTACK_COOLDOWN
+
+
+class Sword(Drawable):
+    xPos: int
+    yPos: int
+    facing: Direction
+    lifetime: int
+
+    _move_speed: int = 1
+    _time_alive: int = 0
+
+    __directions_to_sprite__: dict[Direction, thumby.Sprite]
+
+    def __init__(
+        self,
+        xPos: int,
+        yPos: int,
+        facing: Direction,
+        lifetime: int = 8,
+    ):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.facing = facing
+        self.lifetime = lifetime
+
+        self.__directions_to_sprite__ = {
+            Directions.Up: SpriteBitmaps.Sword.Up.to_sprite(),
+            Directions.Down: SpriteBitmaps.Sword.Down.to_sprite(),
+            Directions.Left: SpriteBitmaps.Sword.Left.to_sprite(),
+            Directions.Right: SpriteBitmaps.Sword.Right.to_sprite(),
+        }
+        self._sprite = self.__directions_to_sprite__[facing]
+        self._sprite.x = self.xPos
+        self._sprite.y = self.yPos
+
+    def expired(self) -> bool:
+        return self._time_alive > self.lifetime
+
+    def step(self):
+        self._time_alive += 1
+
+        if self.facing == Directions.Up:
+            self.yPos -= self._move_speed
+        elif self.facing == Directions.Down:
+            self.yPos += self._move_speed
+        elif self.facing == Directions.Left:
+            self.xPos -= self._move_speed
+        elif self.facing == Directions.Right:
+            self.xPos += self._move_speed
+
+        self._sprite.x = self.xPos
+        self._sprite.y = self.yPos
 
 
 ######################### Main loop #########################
-thumby.display.setFPS(60)
+thumby.display.setFPS(GAME_SPEED)
 
 player = Player(0, 0, Directions.Down)
 start_x = int((thumby.display.width / 2) - int(player.width() / 2))
 start_y = int(thumby.display.height / 2) - int(player.height() / 2)
 player.xPos = start_x
 player.yPos = start_y
+
+swords: list[Sword] = []
 
 while True:
     t0 = time.ticks_ms()  # Get time (ms)
@@ -108,6 +202,16 @@ while True:
         player.move(Directions.Left)
     if thumby.buttonR.pressed():
         player.move(Directions.Right)
+    if thumby.buttonA.pressed():
+        player.attack()
 
+    for sword in swords:
+        sword.step()
+        sword.draw()
+
+        if sword.expired():
+            swords.remove(sword)
+
+    player.step()
     player.draw()
     thumby.display.update()
